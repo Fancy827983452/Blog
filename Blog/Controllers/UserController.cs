f2af9f06b3ce9f354eb4a6aec4b3436513d8a07f
wy3184708959 <3184708959@qq.com>
2017-05-12 21:25:57 +0800

    嘻嘻

---
+++ Blog/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Write()
+        {
+            return View();
+        }
+        public ActionResult EditInfo()
+        {
+            return View();
+        }
+        public ActionResult EditPassword()
+        {
+            return View();
+        }
+        public ActionResult Focus()
+        {
+            return View();
+        }
+        public ActionResult Message()
+        {
+            return View();
+        }
+        public ActionResult Fans()
+        {
+            return View();
+        }
+    }
+}
