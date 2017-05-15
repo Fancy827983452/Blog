575c5f885244b10c3fa98b541a9527173b1f0201
vikii1225 <1043646272@qq.com>
2017-05-15 10:19:23 +0800

    admin lock unlock

---
+++ Blog/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Lock()
+        {
+            return View();
+        }
+        public ActionResult UnLock()
+        {
+            return View();
+        }
+        public ActionResult DeleteBlogs()
+        {
+            return View();
+        }
+        public ActionResult RecommendBlogs()
+        {
+            return View();
+        }
+        
+    }
+}
