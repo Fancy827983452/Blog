abf40f8ef8ffc64c36011bd292014d59252855eb
wy3184708959 <3184708959@qq.com>
2017-05-15 09:04:14 +0800

    Layout修改导航栏

---
+++ Blog/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DeleteBlog()
+        {
+            return View();
+        }
+  
+    }
+}
