7cfa39d5d7cfd39e18e1d898b51e6e92b5f12f7a
Fancy827983452 <827983452@qq.com>
2017-05-15 09:08:00 +0800

    新增UEditor

---
+++ Blog/Models/Blog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Blog
+    {
+        [Key]
+        [DisplayName("博文ID")]
+        [Required]
+        public string BlogID { get; set; }
+
+        [DisplayName("博文标题")]
+        [Required]
+        public string BlogTitle { get; set; }
+
+        [DisplayName("博文内容")]
+        [Required]
+        public string BlogContent { get; set; }
+
+        [DisplayName("博文分类")]
+        public string Classification { get; set; }
+
+        [DisplayName("博主ID")]
+        public string BloggerID { get; set; }
+
+        [DisplayName("创建时间")]
+        [DataType(DataType.DateTime)]
+        public string CreateTime { get; set; }
+
+        [DisplayName("修改时间")]
+        [DataType(DataType.DateTime)]
+        public string ModifyTime { get; set; }
+    }
+}
