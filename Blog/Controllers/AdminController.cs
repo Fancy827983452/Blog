1c5f3a451fa1616bc4d95f8b5d415bed7a46b169
Fancy827983452 <827983452@qq.com>
2017-05-17 09:57:02 +0800

    新增博文管理，可查看博文

--- Blog/Controllers/AdminController.cs
+++ Blog/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog.DatabaseContext;
+using Blog.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,8 +8,10 @@ using System.Web.Mvc;
 
 namespace Blog.Controllers
 {
+
     public class AdminController : Controller
     {
+        private BlogDbContext db = new BlogDbContext();
         // GET: Admin
         public ActionResult Index()
         {
@@ -15,7 +19,15 @@ namespace Blog.Controllers
         }
         public ActionResult ViewBlogs()
         {
-            return View();
+            List<Models.Blog> blogModels = db.Blogs.ToList();//显示数据库中所有的博文
+            if (blogModels.Count != 0)
+            {
+                return View(blogModels);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult Lock()
