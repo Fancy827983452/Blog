86f57687b3b1125b22c8815a61d0e611069877dd
vikii1225 <1043646272@qq.com>
2017-05-18 16:44:28 +0800

    reward

--- Blog/Content/Reward/js/reward.js
+++ Blog/Content/Reward/js/reward.js
@@ -1,12 +1,4 @@
-﻿$(function () {
-    $(".pay_item").click(function () {
-        $(this).addClass('checked').siblings('.pay_item').removeClass('checked');
-        var dataid = $(this).attr('data-id');
-        $(".shang_payimg img").attr("src", "~/Content/Reward/img/" + dataid + "img.jpg");
-        $("#shang_pay_txt").text(dataid == "alipay" ? "支付宝" : "微信");
-    });
-});
-function dashangToggle() {
+﻿function dashangToggle() {
     $(".hide_box").fadeToggle();
     $(".shang_box").fadeToggle();
 }
