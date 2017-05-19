7546b3b30e29bd3a22b31eb3d2e53c3d97a5be3c
wy3184708959 <3184708959@qq.com>
2017-05-19 09:51:11 +0800

    还原关注留言

--- Blog/Models/DatabaseContext/BlogDbContext.cs
+++ Blog/Models/DatabaseContext/BlogDbContext.cs
@@ -18,9 +18,6 @@ namespace Blog.DatabaseContext
         public System.Data.Entity.DbSet<Blog.Models.Blog> Blogs { get; set; }
         public System.Data.Entity.DbSet<Blog.Models.Comment> Comments { get; set; }
         public System.Data.Entity.DbSet<Blog.Models.Like> Likes { get; set; }
-        public System.Data.Entity.DbSet<Blog.Models.Focus>  Focuses { get; set; }
-        public System.Data.Entity.DbSet<Blog.Models.Message> Messages { get; set; }
-        public System.Data.Entity.DbSet<Blog.Models.Recommend> Recommends { get; set; }
 
     }
 }
